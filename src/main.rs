ea5b6ca4c69dd4e4bafaf2dc0434a1b0ecad2dd1
Bernard <b.mullerjnr@gmail.com>
2025-03-16 21:32:20 +0200

    feat: csv bytes string parser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,38 @@
-use axum::{routing::get, routing::post, Router,body::{Bytes, Body}, http::{Method, HeaderMap, }};
+use std::error::Error;
+use std::io::Cursor;
+// use chrono::NaiveDate;
+use serde::{Deserialize, Serialize};
+use axum::{ routing::post, Router,body::Bytes, 
+// http::{ HeaderMap, }
+};
+
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct MoodEntry {
+    full_date: String,
+    date: String,
+    weekday: String,
+    time: String,
+    mood: String,
+    activities: String,
+    note_title: String,
+    note: String,
+}
+
+pub fn parse_csv_string(csv_data: &str) -> Result<Vec<MoodEntry>, Box<dyn Error>> {
+    let mut rdr = csv::ReaderBuilder::new()
+        .flexible(true)
+        .trim(csv::Trim::All)
+        .from_reader(Cursor::new(csv_data));
+    
+    let mut entries = Vec::new();
+    for result in rdr.deserialize() {
+        let record: MoodEntry = result?;
+        entries.push(record);
+    }
+    
+    Ok(entries)
+}
 
 #[tokio::main]
 async fn main() {
@@ -12,6 +46,12 @@ async fn main() {
 }
 
 async fn handler(body: Bytes) {
-    println!("{:?}", body);
+
+    let body_str = String::from_utf8(body.to_vec()).unwrap();
+
+    let entries = parse_csv_string(&body_str).unwrap();
+
+    let first_entry = entries.first().unwrap();
+    println!("{:?}: {:?}", first_entry.full_date, first_entry.mood);
 }
 
