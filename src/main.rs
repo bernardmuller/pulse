ce62fbd154c6c09ea0bfe21bab8a41b712cefd0f
Bernard <b.mullerjnr@gmail.com>
2025-03-16 12:39:05 +0200

    feat: basic webhook to handle daylio log from ios shortcut

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,17 @@
-pub mod url_builder;
-
-use anyhow::Result;
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Post {
-    id: i32,
-    userId: i32,
-    title: String,
-    body: String,
-}
+use axum::{routing::get, routing::post, Router,body::{Bytes, Body}, http::{Method, HeaderMap, }};
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    println!("Sending request to JSONPlaceholder API...");
+async fn main() {
+    let app = Router::new().route("/log", post(handler));
 
-    // Create a client
-    let client = reqwest::Client::new();
-
-    // Send a GET request
-    let response = client
-        .get("https://jsonplaceholder.typicode.com/posts/1")
-        .send()
-        .await?;
-
-    // Check if the request was successful
-    if response.status().is_success() {
-        println!("Request successful!");
-
-        // Parse the JSON response
-        let post: Post = response.json().await?;
-
-        // Print the post
-        println!("Post ID: {}", post.id);
-        println!("User ID: {}", post.userId);
-        println!("Title: {}", post.title);
-        println!("Body: {}", post.body);
-    } else {
-        println!("Request failed with status: {}", response.status());
-    }
-
-    Ok(())
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
+
+async fn handler(body: Bytes) {
+    println!("{:?}", body);
+}
+
